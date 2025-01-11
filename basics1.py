2d7bcfa1e72f1115d56da24518117d7f5b9d893b
aagamaar <aagamaar01@gmail.com>
2025-01-11 20:17:21 +0530

    Added some more list functions

--- basics1.py
+++ basics1.py
@@ -21,3 +21,17 @@ list1.remove(11)
 print(list1)
 #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+#pop
+list1.pop(3)
+print(list1)
+#[1, 2, 3, 5, 6, 7, 8, 9, 10]
+
+#index
+l=list1.index(5)
+print(l)
+#3 is the index of the number 5 from the list
+
+#count
+print(list1.count(7))
+1
+
