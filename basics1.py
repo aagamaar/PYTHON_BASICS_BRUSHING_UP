701bb761e0dd1c845a52f51d2c54f3723097fb04
aagamaar <aagamaar01@gmail.com>
2025-01-11 20:06:42 +0530

    Added answers as comments

--- basics1.py
+++ basics1.py
@@ -3,13 +3,21 @@ list1= [1,2,3,4,5,6,]
 #append
 list1.append(7)
 print(list1)
+#[1, 2, 3, 4, 5, 6, 7]
 
 #extend
 list1.extend([8,9,10])
 print(list1)
+#[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 
 #insert
 list1.insert(10,11)
 print(list1)
+#[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 #remove
+list1.remove(11)
+print(list1)
+#[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
